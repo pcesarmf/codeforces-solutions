cd89d9de725c0710afaf1a8e1a29c7d6ef3570af
pcesarmf <pedrocesar1410@gmail.com>
2020-06-25 13:25:18 -0300

    Update C++ template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,12 +1,22 @@
+// <Link to problem>
+
 #include <iostream>
 
 using namespace std;
 
+void solve() {
+	;
+}
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
 	int t;
 	cin >> t;
 	while (t--) {
-		// Code
+		solve();
 	}
+
 	return 0;
 }
